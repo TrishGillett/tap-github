ef8aff236abcffe6f7cc60d202592d4b2fbcb384
Trish Gillett-Kawamoto <trish.gillett@shopify.com>
2024-07-28 11:46:14 -0600

    initial changes

--- tap_github/authenticator.py
+++ tap_github/authenticator.py
@@ -13,27 +13,8 @@ from singer_sdk.authenticators import APIAuthenticatorBase
 from singer_sdk.streams import RESTStream
 
 
-class TokenRateLimit:
-    """A class to store token rate limiting information."""
-
-    DEFAULT_RATE_LIMIT = 5000
-    # The DEFAULT_RATE_LIMIT_BUFFER buffer serves two purposes:
-    # - keep some leeway and rotate tokens before erroring out on rate limit.
-    # - not consume all available calls when we rare using an org or user token.
-    DEFAULT_RATE_LIMIT_BUFFER = 1000
-
-    def __init__(self, token: str, rate_limit_buffer: Optional[int] = None):
-        """Init TokenRateLimit info."""
-        self.token = token
-        self.rate_limit = self.DEFAULT_RATE_LIMIT
-        self.rate_limit_remaining = self.DEFAULT_RATE_LIMIT
-        self.rate_limit_reset: Optional[int] = None
-        self.rate_limit_used = 0
-        self.rate_limit_buffer = (
-            rate_limit_buffer
-            if rate_limit_buffer is not None
-            else self.DEFAULT_RATE_LIMIT_BUFFER
-        )
+class TokenManager:
+    """A class to store a token's attributes and state."""
 
     def update_rate_limit(self, response_headers: Any) -> None:
         self.rate_limit = int(response_headers["X-RateLimit-Limit"])
@@ -55,6 +36,25 @@ class TokenRateLimit:
         ):
             return False
         return True
+
+
+class PersonalTokenManager(TokenManager):
+    """A class to store token rate limiting information."""
+
+    DEFAULT_RATE_LIMIT = 5000
+    # The DEFAULT_RATE_LIMIT_BUFFER buffer serves two purposes:
+    # - keep some leeway and rotate tokens before erroring out on rate limit.
+    # - not consume all available calls when we rare using an org or user token.
+    DEFAULT_RATE_LIMIT_BUFFER = 1000
+
+    def __init__(self, token: str, rate_limit_buffer: Optional[int] = None):
+        """Init PersonalTokenRateLimit info."""
+        self.token = token
+        self.rate_limit = self.DEFAULT_RATE_LIMIT
+        self.rate_limit_remaining = self.DEFAULT_RATE_LIMIT
+        self.rate_limit_reset: Optional[int] = None
+        self.rate_limit_used = 0
+        self.rate_limit_buffer = rate_limit_buffer or self.DEFAULT_RATE_LIMIT_BUFFER
 
 
 def generate_jwt_token(
@@ -110,16 +110,58 @@ def generate_app_access_token(
     return resp.json()["token"]
 
 
+class AppTokenManager(TokenManager):
+    """A class to store an app token's attributes and state, and handle token refreshing"""
+
+    # TODO - add logic to refresh the token
+
+    DEFAULT_RATE_LIMIT = 15000
+    # The DEFAULT_RATE_LIMIT_BUFFER buffer serves two purposes:
+    # - keep some leeway and rotate tokens before erroring out on rate limit.
+    # - not consume all available calls when we rare using an org or user token.
+    DEFAULT_RATE_LIMIT_BUFFER = 1000
+
+    def gen_key(self):
+        if not (github_private_key):
+            self.logger.warning(
+                "GITHUB_APP_PRIVATE_KEY could not be parsed. The expected format is "
+                '":app_id:;;-----BEGIN RSA PRIVATE KEY-----\n_YOUR_P_KEY_\n-----END RSA PRIVATE KEY-----"'
+            )
+            return None
+
+        else:
+            token = generate_app_access_token(
+                self.github_app_id, self.github_private_key, self.github_installation_id or None
+            )
+            return token
+
+    def __init__(self, env_key: str, rate_limit_buffer: Optional[int] = None):
+        """Init PersonalTokenRateLimit info."""
+        parts = env_key.split(";;")
+        self.github_app_id = parts[0]
+        self.github_private_key = (parts[1:2] or [""])[0].replace("\\n", "\n")
+        self.github_installation_id = (parts[2:3] or [""])[0]
+        self.current_token = self.gen_key()
+
+        self.rate_limit = self.DEFAULT_RATE_LIMIT
+        self.rate_limit_remaining = self.DEFAULT_RATE_LIMIT
+        self.rate_limit_reset: Optional[int] = None
+        self.rate_limit_used = 0
+        self.rate_limit_buffer = rate_limit_buffer or self.DEFAULT_RATE_LIMIT_BUFFER
+
+
 class GitHubTokenAuthenticator(APIAuthenticatorBase):
     """Base class for offloading API auth."""
 
-    def prepare_tokens(self) -> Dict[str, TokenRateLimit]:
+    def prepare_tokens(self) -> Dict[str, TokenManager]:
         # Save GitHub tokens
-        available_tokens: List[str] = []
+        rate_limit_buffer = self._config.get("rate_limit_buffer", None)
+
+        found_tokens: Set[str] = set()
         if "auth_token" in self._config:
-            available_tokens = available_tokens + [self._config["auth_token"]]
+            found_tokens = found_tokens.add(self._config["auth_token"])
         if "additional_auth_tokens" in self._config:
-            available_tokens = available_tokens + self._config["additional_auth_tokens"]
+            found_tokens = found_tokens.union(self._config["additional_auth_tokens"])
         else:
             # Accept multiple tokens using environment variables GITHUB_TOKEN*
             env_tokens = [
@@ -131,35 +173,23 @@ class GitHubTokenAuthenticator(APIAuthenticatorBase):
                 self.logger.info(
                     f"Found {len(env_tokens)} 'GITHUB_TOKEN' environment variables for authentication."
                 )
-                available_tokens = env_tokens
+                found_tokens = env_tokens
+
+        tokens_map = {
+            token: PersonalTokenManager(token, rate_limit_buffer) for token in found_tokens
+        }
 
         # Parse App level private key and generate a token
         if "GITHUB_APP_PRIVATE_KEY" in environ.keys():
             # To simplify settings, we use a single env-key formatted as follows:
             # "{app_id};;{-----BEGIN RSA PRIVATE KEY-----\n_YOUR_PRIVATE_KEY_\n-----END RSA PRIVATE KEY-----}"
-            parts = environ["GITHUB_APP_PRIVATE_KEY"].split(";;")
-            github_app_id = parts[0]
-            github_private_key = (parts[1:2] or [""])[0].replace("\\n", "\n")
-            github_installation_id = (parts[2:3] or [""])[0]
+            env_key = environ["GITHUB_APP_PRIVATE_KEY"]
+            rate_limit_buffer = self._config.get("rate_limit_buffer", None)
+            app_token_manager = AppTokenManager(env_key, rate_limit_buffer=rate_limit_buffer)
+            if app_token_manager.current_token is not None:
+                tokens_map[app_token_manager.current_token] = app_token_manager
 
-            if not (github_private_key):
-                self.logger.warning(
-                    "GITHUB_APP_PRIVATE_KEY could not be parsed. The expected format is "
-                    '":app_id:;;-----BEGIN RSA PRIVATE KEY-----\n_YOUR_P_KEY_\n-----END RSA PRIVATE KEY-----"'
-                )
-
-            else:
-                app_token = generate_app_access_token(
-                    github_app_id, github_private_key, github_installation_id or None
-                )
-                available_tokens = available_tokens + [app_token]
-
-        # Get rate_limit_buffer
-        rate_limit_buffer = self._config.get("rate_limit_buffer", None)
-
-        # Dedup tokens and test them
-        filtered_tokens = []
-        for token in list(set(available_tokens)):
+        for token in tokens_map:
             try:
                 response = requests.get(
                     url="https://api.github.com/rate_limit",
@@ -168,8 +198,8 @@ class GitHubTokenAuthenticator(APIAuthenticatorBase):
                     },
                 )
                 response.raise_for_status()
-                filtered_tokens.append(token)
             except requests.exceptions.HTTPError:
+                del tokens_map[token]
                 msg = (
                     f"A token was dismissed. "
                     f"{response.status_code} Client Error: "
@@ -178,13 +208,7 @@ class GitHubTokenAuthenticator(APIAuthenticatorBase):
                 self.logger.warning(msg)
 
         self.logger.info(f"Tap will run with {len(filtered_tokens)} auth tokens")
-
-        # Create a dict of TokenRateLimit
-        # TODO - separate app_token and add logic to refresh the token
-        # using generate_app_access_token.
-        return {
-            token: TokenRateLimit(token, rate_limit_buffer) for token in filtered_tokens
-        }
+        return tokens_map
 
     def __init__(self, stream: RESTStream) -> None:
         """Init authenticator.
@@ -197,7 +221,7 @@ class GitHubTokenAuthenticator(APIAuthenticatorBase):
         self.tap_name: str = stream.tap_name
         self._config: Dict[str, Any] = dict(stream.config)
         self.tokens_map = self.prepare_tokens()
-        self.active_token: Optional[TokenRateLimit] = (
+        self.active_token: Optional[PersonalTokenManager] = (
             choice(list(self.tokens_map.values())) if len(self.tokens_map) else None
         )
 
