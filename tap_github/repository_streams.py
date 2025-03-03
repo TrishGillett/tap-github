6a7baf510ed534a6ad601d923c77bac4fc360428
Trish Gillett-Kawamoto <trish.gillett@shopify.com>
2025-03-03 03:39:21 -0600

    crazy idea, can we pass it in context?

--- tap_github/repository_streams.py
+++ tap_github/repository_streams.py
@@ -1249,6 +1249,7 @@ class PullRequestsStream(GitHubRestStream):
                 "repo_id": context["repo_id"],
                 "pull_number": record["number"],
                 "pull_id": record["id"],
+                "updated_at": record["updated_at"],
             }
         return {
             "pull_number": record["number"],
@@ -1448,6 +1449,7 @@ class PullRequestDiffsStream(GitHubRestStream):
     primary_keys: ClassVar[list[str]] = ["pull_id"]
     parent_stream_type = PullRequestsStream
     ignore_parent_replication_key = True
+    replication_key = "updated_at"
     state_partitioning_keys: ClassVar[list[str]] = ["repo", "org"]
     # Known Github API errors
     tolerated_http_errors: ClassVar[list[int]] = [404, 406, 422, 502]
